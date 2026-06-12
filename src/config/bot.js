292b7ff896286803902d26db7ad4f730419eebf8
zewtspubg <zewtspubg@gmail.com>
2026-06-12 16:46:54 +0300

    Change bot activity type from Playing to Streaming

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Juicy is here!🧃",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
