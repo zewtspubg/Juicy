5ce8cadf8394afe4bee1b26903b8c2aee3369a60
zewtspubg <zewtspubg@gmail.com>
2026-06-12 19:07:03 +0300

    Increase fish cooldown duration

--- src/commands/Economy/fish.js
+++ src/commands/Economy/fish.js
@@ -5,7 +5,7 @@ import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHan
 import { MessageTemplates } from '../../utils/messageTemplates.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const FISH_COOLDOWN = 60; 
+const FISH_COOLDOWN = 60*120*1000; 
 const BASE_MIN_REWARD = 300;
 const BASE_MAX_REWARD = 900;
 const FISHING_ROD_MULTIPLIER = 1.5;
